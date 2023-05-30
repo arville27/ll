9c028b22cf53a7c80bedceba6b8a87e7eb9830fd
arville27 <arville27@users.noreply.github.com>
2023-05-30 19:47:53 +0700

    Change application name

--- packages/ui/src/Layout.tsx
+++ packages/ui/src/Layout.tsx
@@ -1,29 +1,34 @@
 import { HTMLAttributes, PropsWithChildren, ReactNode } from 'react';
 import { NavigationBar, NavigationBarProps } from './NavigationBar';
 import { Notifications } from '@mantine/notifications';
+import Head from 'next/head';
 
 export interface LayoutProps extends HTMLAttributes<HTMLElement> {
   footer?: ReactNode;
+  pageTitle: string;
   navbarProp: NavigationBarProps;
 }
 
 export const Layout: React.FC<PropsWithChildren<LayoutProps>> = ({
   footer,
   children,
+  pageTitle = 'Little Learners Attendance System',
   navbarProp,
   ...layoutAttribute
 }) => {
-  {
-    /* <Notifications position="top-right" limit={3} /> */
-  }
   return (
-    <div className="min-h-screen grid grid-rows-[auto_1fr_auto]">
-      <NavigationBar {...navbarProp} />
-      <main {...layoutAttribute}>
-        <Notifications zIndex={200} position="top-right" />
-        {children}
-      </main>
-      <footer className="w-full flex justify-center">{footer}</footer>
-    </div>
+    <>
+      <Head>
+        <title>{pageTitle}</title>
+      </Head>
+      <div className='min-h-screen grid grid-rows-[auto_1fr_auto]'>
+        <NavigationBar {...navbarProp} />
+        <main {...layoutAttribute}>
+          <Notifications zIndex={200} position='top-right' />
+          {children}
+        </main>
+        <footer className='w-full flex justify-center'>{footer}</footer>
+      </div>
+    </>
   );
 };
--- packages/ui/src/NavigationBar.tsx
+++ packages/ui/src/NavigationBar.tsx
@@ -18,25 +18,20 @@ import littleLearnerLogo from './assets/logo.png';
 
 const useStyles = createStyles((theme) => ({
   link: {
-    display: 'flex',
-    gap: '0.5rem',
-    alignItems: 'center',
-    lineHeight: 1,
-    padding: '8px 12px',
-    borderRadius: theme.radius.sm,
-    textDecoration: 'none',
-    color:
-      theme.colorScheme === 'dark'
-        ? theme.colors.dark[0]
-        : theme.colors.gray[7],
-    fontSize: theme.fontSizes.sm,
-    fontWeight: 500,
+    'display': 'flex',
+    'gap': '0.5rem',
+    'alignItems': 'center',
+    'lineHeight': 1,
+    'padding': '8px 12px',
+    'borderRadius': theme.radius.sm,
+    'textDecoration': 'none',
+    'color': theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
+    'fontSize': theme.fontSizes.sm,
+    'fontWeight': 500,
 
     '&:hover': {
       backgroundColor:
-        theme.colorScheme === 'dark'
-          ? theme.colors.dark[6]
-          : theme.colors.gray[0],
+        theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
     },
   },
 
@@ -46,8 +41,7 @@ const useStyles = createStyles((theme) => ({
         variant: 'light',
         color: theme.primaryColor,
       }).background,
-      color: theme.fn.variant({ variant: 'light', color: theme.primaryColor })
-        .color,
+      color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
     },
   },
 
@@ -75,43 +69,41 @@ export function NavigationBar({ links }: NavigationBarProps) {
       href={item.link}
       className={cx(classes.link, {
         [classes.linkActive]: router.pathname === item.link,
-      })}
-    >
+      })}>
       {item.icon}
       {item.label}
     </Link>
   ));
   return (
-    <Header height={60} className="z-[100] sticky top-0">
-      <Container size="xl" className="flex justify-between items-center h-full">
+    <Header height={60} className='z-[100] sticky top-0'>
+      <Container size='xl' className='flex justify-between items-center h-full'>
         <Group>
           <Image
             priority
             src={littleLearnerLogo}
             height={40}
             width={40}
-            alt="Little Learner Logo"
+            alt='Little Learner Logo'
           />
           <Text
-            className="fw-600 tracking-wide"
+            className='fw-600 tracking-wide'
             sx={{
               color:
                 theme.colorScheme === 'dark'
                   ? theme.colors.dark[0]
                   : theme.colors.gray[7],
-            }}
-          >
-            Little Learners
+            }}>
+            Little Learners Attendance System
           </Text>
         </Group>
-        <Group spacing={8} className="hidden sm:flex">
+        <Group spacing={8} className='hidden sm:flex'>
           {items}
-          <ThemeSwitch size="lg" />
+          <ThemeSwitch size='lg' />
         </Group>
 
-        <Menu opened={opened} onChange={setOpened} shadow="md" width={200}>
+        <Menu opened={opened} onChange={setOpened} shadow='md' width={200}>
           <Menu.Target>
-            <Burger opened={opened} className={classes.burger} size="sm" />
+            <Burger opened={opened} className={classes.burger} size='sm' />
           </Menu.Target>
 
           <Menu.Dropdown>
@@ -121,10 +113,10 @@ export function NavigationBar({ links }: NavigationBarProps) {
 
             <Menu.Label>Others</Menu.Label>
             <Menu.Label>
-              <Group align="center">
+              <Group align='center'>
                 <IconEye size={14} />
-                <Text fz="xs">Dark mode</Text>
-                <ThemeSwitch size="md" />
+                <Text fz='xs'>Dark mode</Text>
+                <ThemeSwitch size='md' />
               </Group>
             </Menu.Label>
           </Menu.Dropdown>
